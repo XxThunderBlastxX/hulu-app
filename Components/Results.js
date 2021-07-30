d2d3ac40e71bee47c2c674ef3272c454f728f069
xXThunderBlastxX <koustavmondal55@gmail.com>
2021-07-30 21:12:31 +0530

    underDev

--- Components/Results.js
+++ Components/Results.js
@@ -1,11 +1,21 @@
 import Thumbnail from './Thumbnail'
 
-function Results() {
+function Results({results}) {
     return (
         <div>
+            {/*{results.map(result =>(*/}
+            {/*    <Thumbnail key={result.key} result={result}/>*/}
+            {/*))}*/}
 
+            <Thumbnail/>
+
+            {/*{results.map((result)=>(*/}
+            {/*    <Thumbnail key={result.key} result={result}/>*/}
+            {/*))}*/}
         </div>
     );
 }
+
+// <Thumbnail key={results.id} result={result}/>
 
 export default Results;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  mode: 'jit',
+   // These paths are just examples, customize them to match your project structure
+
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': "var(--background-color)",
+        'back-color': "#06202a",
+      }
+    },
   },
   variants: {
     extend: {},
